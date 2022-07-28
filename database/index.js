38c7aca4a437a4b63885e49182f00051cfae6018
pride2A <109552072+pride2A@users.noreply.github.com>
2022-07-28 19:03:58 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
+      host : 'localhost ',
+      port : '3306',
       user : 'id19294439_rootlogin',
       password : '=jJZ]?n_TU/04Ad4',
       database : 'id19294439_root'
